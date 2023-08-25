737fdaf7b4c1a028e714e2cb985472b89bd009b3
ciscomsk <kuznetzoff@gmail.com>
2023-08-25 14:23:24 +0300

    Added hw for lesson 7

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,54 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	//var size byte
+	//getBoardSizeV1(&size)
+
+	size := getBoardSizeV2()
+	drawBoard(size)
+}
+
+func getBoardSizeV1(size *int) *int {
+	fmt.Println("Enter a chessboard size: ")
+	_, err := fmt.Scanf("%d", size)
+	if err != nil {
+		return nil
+	}
+
+	return size
+}
+
+func getBoardSizeV2() int {
+	var size int
+
+	fmt.Println("Enter a chessboard size: ")
+	_, err := fmt.Scanf("%d", &size)
+	if err != nil {
+		return 0
+	}
+
+	return size
+}
+
+func drawBoard(size int) {
+	for i := 1; i <= size; i++ {
+		for j := 1; j <= size; j++ {
+			if i%2 == 0 {
+				if j%2 == 0 {
+					fmt.Print(" ")
+				} else {
+					fmt.Print("#")
+				}
+			} else {
+				if j%2 == 0 {
+					fmt.Print("#")
+				} else {
+					fmt.Print(" ")
+				}
+			}
+		}
+		fmt.Print("\n")
+	}
 }
